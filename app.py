7a670a11a92640c619a48833e03a9a7adbd4e308
waseemqayd5-spec <waseemqayd5@gmail.com>
2026-06-16 21:20:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -766,7 +766,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ViralShield AI | نظام التسويق الذكي</title>
+    <title>ViralShield AI |  نظام التسويق الذكي إعدادوتصميم م/ وسيم الحميدي </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
